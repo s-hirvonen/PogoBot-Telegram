c2e37c091127ccbf63bfcd6a0b95c1bd5cd91249
Samuli Hirvonen <hirvonensamuli@gmail.com>
2016-08-04 17:55:59 +0300

    Load default watchlist from the config

--- bot.js
+++ bot.js
@@ -6,14 +6,18 @@ module.exports = function(config) {
 
     var bot = new TelegramBot(config.api_token, {polling: true});
 
-    var pokemon = ['Bulbasaur', 'Charmander', 'Pikachu'];
+    var pokemon = config.watchlist;
 
+    // Add command
+    // Accepts a space or comma-separated list of Pokemen to watch
     bot.onText(/\/add (.+)/, function(msg, match) {
         var toAdd = splitCommandArgs(match[1]);
         pokemon.push(toAdd);
         bot.sendMessage(msg.from.id, 'Added ' + toAdd.join(', ') + '!');
     });
 
+    // Remove command
+    // Accepts a space or comma-separated list of Pokemen to unwatch
     bot.onText(/\/remove (.+)/, function(msg, match) {
         var toRemove = splitCommandArgs(match[1]);
 
@@ -24,6 +28,8 @@ module.exports = function(config) {
         bot.sendMessage(msg.from.id, printWatchlist());
     });
 
+    // List command
+    // Lists all the pokemen currently on the watchlist
     bot.onText(/\/list/, function(msg) {
         bot.sendMessage(msg.from.id, printWatchlist());
     });
